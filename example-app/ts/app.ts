d09c2925b2472274629dfd60fb87c7adb1e2194f
Igor Krivanov <igor.krivanov@rangle.io>
2015-09-23 10:59:28 -0400

    Fix compile errors on the example app

--- example-app/ts/app.ts
+++ example-app/ts/app.ts
@@ -1,8 +1,12 @@
 import {bootstrap, bind} from 'angular2/angular2';
 import {TodoList} from './components/todo-list';
 import {TodoStore} from './stores/todo-store';
-import { AppViewListener } from 'angular2/src/core/compiler/view_listener.js';
-import { DebugElementViewListener } from 'angular2/src/core/debug/debug_element_view_listener';
+// import { AppViewListener } from 'angular2/src/core/compiler/view_listener.js';
+// import { DebugElementViewListener } from 'angular2/src/core/debug/debug_element_view_listener';
+
+var AppViewListener = require('angular2/src/core/compiler/view_listener').AppViewListener;
+var DebugElementViewListener = require('angular2/src/core/debug/debug_element_view_listener').DebugElementViewListener;
+
 
 let appRefPromise = bootstrap(TodoList, [
   bind(TodoStore).toClass(TodoStore),
