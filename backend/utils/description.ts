6f274da418e638a32eb93c6e89472542f2b73fbc
“Sumit <er.sumitarora@gmail.com>
2016-01-28 12:50:55 -0500

    added more component descriptions

--- backend/utils/description.ts
+++ backend/utils/description.ts
@@ -1,6 +1,11 @@
 import { DebugElement_ as DebugElement }
        from 'angular2/src/core/debug/debug_element';
 
+export interface Property {
+  key: string;
+  value: string;
+}
+
 export abstract class Description {
 
   public static getComponentDescription(compEl: DebugElement): Object[] {
@@ -8,42 +13,119 @@ export abstract class Description {
     const componentInstance = compEl.componentInstance || {};
     const constructor =  componentInstance.constructor;
     const constructorName = constructor.name;
-    let componentName = constructorName !== 'Object' ?
+    const componentName = constructorName !== 'Object' ?
       constructorName : compEl.nativeElement.tagName;
-    let description = [];
-    // console.log(componentName);
+    let description: Array<Property> = new Array<Property>();
+
     switch (componentName) {
       case 'RouterLink':
         description =  Description._getRouterLinkDesc(compEl);
         break;
+      case 'RouterOutlet':
+        description =  Description._getRouterOutletDesc(compEl);
+        break;
       case 'NgSelectOption':
         description = Description._getSelectOptionDesc(compEl);
         break;
+      case 'NgControlName':
+        description = Description._getControlNameDesc(compEl);
+        break;
+      case 'NgModel':
+        description = Description._getNgModelDesc(compEl);
+        break;
+      case 'NgForm':
+        description = Description._getNgFormDesc(compEl);
+        break;
+      case 'NgSwitch':
+        description = Description._getNgSwitchDesc(compEl);
+        break;
+      case 'NgSwitchWhen':
+        description = Description._getNgSwitchWhenDesc(compEl);
+        break;
+      case 'NgSwitchDefault':
+        description = Description._getNgSwitchWhenDesc(compEl);
+        break;
       default:
+        description = [
+          { key: 'name', value: componentName },
+        ];
         break;
     }
     return description;
   }
 
-  private static _getRouterLinkDesc(compEl: DebugElement): Object[] {
-    let element: HTMLElement = <HTMLElement>(compEl.nativeElement);
-    let href = element.getAttribute('href');
-    let htmlText = element.innerText;
-    return [{
-      key: 'href',
-      val: href
-    }, {
-      key: 'htmlText',
-      val: htmlText
-    }];
+  private static _getRouterLinkDesc(compEl: DebugElement): Array<Property> {
+    const element: HTMLElement = <HTMLElement>(compEl.nativeElement);
+    const href: string = element.getAttribute('href');
+    const htmlText: string = element.innerText;
+
+    return [
+      { key: 'href', value: href },
+      { key: 'htmlText', value: htmlText }
+    ];
   }
 
-  private static _getSelectOptionDesc(compEl: DebugElement): Object[] {
+  private static _getSelectOptionDesc(compEl: DebugElement): Array<Property> {
     const elem = <HTMLElement>compEl.nativeElement;
-    return [{
-      key: 'value',
-      val: elem.getAttribute('value')
-    }];
+    return [
+      { key: 'label', value: elem.innerText },
+      { key: 'value', value: elem.getAttribute('value') }
+    ];
   }
+
+  private static _getControlNameDesc(compEl: DebugElement): Array<Property> {
+    const componentInstance = compEl.componentInstance;
+    return [
+      { key: 'name', value: componentInstance.name },
+      { key: 'value', value: componentInstance.value },
+      { key: 'valid', value: componentInstance.valid }
+    ];
+  }
+
+  private static _getNgFormDesc(compEl: DebugElement): Array<Property> {
+    const componentInstance = compEl.componentInstance;
+    return [
+      { key: 'status', value: componentInstance.form.status },
+      { key: 'dirty', value: componentInstance.form.dirty }
+    ];
+  }
+
+  private static _getRouterOutletDesc(compEl: DebugElement): Array<Property> {
+    const componentInstance = compEl.componentInstance;
+    return [
+      { key: 'name', value: componentInstance.name || ''},
+      { key: 'hostComponent',
+      value: componentInstance._componentRef.componentType.name },
+    ];
+  }
+
+  private static _getNgSwitchDesc(compEl: DebugElement): Array<Property> {
+    const componentInstance = compEl.componentInstance;
+    return [
+      { key: 'useDefault', value: componentInstance._useDefault },
+      { key: 'switchValue', value: componentInstance._switchValue },
+      { key: 'valuesCount', value: componentInstance._valueViews.size }
+    ];
+  }
+
+  private static _getNgSwitchWhenDesc(compEl: DebugElement): Array<Property> {
+    const componentInstance = compEl.componentInstance;
+    console.log(componentInstance);
+    return [
+      { key: 'value', value: componentInstance._value }
+    ];
+  }
+
+  private static _getNgModelDesc(compEl: DebugElement): Array<Property> {
+    const componentInstance = compEl.componentInstance;
+    return [
+      { key: 'model', value: componentInstance.name },
+      { key: 'value', value: componentInstance.value },
+      { key: 'viewModel', value: componentInstance.viewModel },
+      { key: 'controlStatus', value: componentInstance._control.status },
+      { key: 'dirty', value: componentInstance.dirty }
+    ];
+  }
+
 
 }
