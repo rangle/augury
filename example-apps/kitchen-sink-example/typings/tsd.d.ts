b8a34994cfe2234a53af9df03dfeec6ddeabe463
“Sumit <er.sumitarora@gmail.com>
2016-02-16 11:59:48 -0500

    updating version to beta.6

--- example-apps/kitchen-sink-example/typings/tsd.d.ts
+++ example-apps/kitchen-sink-example/typings/tsd.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="../node_modules/definition-header/dist/index.d.ts" />
 /// <reference path="../node_modules/immutable/dist/immutable.d.ts" />
 /// <reference path="../node_modules/reflect-metadata/reflect-metadata.d.ts" />
+/// <reference path="../node_modules/angular2/typings/browser.d.ts"/>
