996c3686777363cdaca3b4430e2edf164fcc1586
“Sumit <er.sumitarora@gmail.com>
2016-03-17 09:44:20 -0400

    adding change detection information

--- src/backend/adapters/angular2.ts
+++ src/backend/adapters/angular2.ts
@@ -36,6 +36,8 @@ import {DirectiveResolver} from '../directive-resolver';
 import {Observable} from 'rxjs/Observable';
 import {Subject} from 'rxjs/Subject';
 
+import {ChangeDetectionStrategy} from 'angular2/core';
+
 export class Angular2Adapter extends BaseAdapter {
   _tree: any = {};
   _onEventDone: any;
@@ -116,6 +118,7 @@ export class Angular2Adapter extends BaseAdapter {
     const input = this._getComponentInput(debugEl);
     const output = this._getComponentOutput(debugEl);
     const dependencies = this._getComponentDependencies(debugEl);
+    const changeDetection = this._getComponentCD(debugEl);
 
     description.unshift({
       key: 'b-id',
@@ -131,7 +134,8 @@ export class Angular2Adapter extends BaseAdapter {
       output,
       isSelected: false,
       isOpen: true,
-      dependencies
+      dependencies,
+      changeDetection
     };
   }
 
@@ -194,6 +198,16 @@ export class Angular2Adapter extends BaseAdapter {
               genericMatch;
 
     return f.call(el, selector);
+  }
+
+  _getComponentCD(compEl: any) {
+    let changeDetection;
+    const metadata = Reflect.getOwnMetadata('annotations',
+      compEl.componentInstance.constructor);
+    if (metadata && metadata.length > 0) {
+      changeDetection = metadata[0].changeDetection;
+    }
+    return ChangeDetectionStrategy[changeDetection];
   }
 
   _getComponentDependencies(compEl: any) {
