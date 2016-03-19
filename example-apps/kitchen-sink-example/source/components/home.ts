a6a461a8588b80b67efbb62ebe023c9a02e26fd3
Vanessa Yuen <vanessa.yh.yuen@gmail.com>
2016-03-19 12:07:01 -0400

    chore: upgrade to beta.11  - resolve all the zone.js related issues from beta.10  - upgrade example app to beta.11 too

--- example-apps/kitchen-sink-example/source/components/home.ts
+++ example-apps/kitchen-sink-example/source/components/home.ts
@@ -1,9 +1,7 @@
-import {Component, View} from 'angular2/core';
+import {Component} from 'angular2/core';
 
 @Component({
-  selector: 'home'
-})
-@View({
+  selector: 'home',
   template: `
   <div>
     <h4>Hello There!!</h4>
