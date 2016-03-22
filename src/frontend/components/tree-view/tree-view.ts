8d4ab3b47551c9078f4ccab9f157b36ae9215961
Vanessa Yuen <vanessa.yh.yuen@gmail.com>
2016-03-22 18:34:23 -0400

    actually putting search back cos I could not make up my damn mind.

--- src/frontend/components/tree-view/tree-view.ts
+++ src/frontend/components/tree-view/tree-view.ts
@@ -6,13 +6,12 @@ import {ComponentDataStore}
   from '../../stores/component-data/component-data-store';
 import {UserActionType}
   from '../../actions/action-constants';
-import {TreeSearch} from '../tree-search/tree-search';
 
 @Component({
   selector: 'bt-tree-view',
   properties: ['tree: tree'],
   templateUrl: 'src/frontend/components/tree-view/tree-view.html',
-  directives: [NgFor, NodeItem, TreeSearch]
+  directives: [NgFor, NodeItem]
 })
 /**
  * The Tree View
@@ -22,13 +21,21 @@ export class TreeView {
 
   private tree: any;
   private nativeElement: HTMLElement;
+  private searchIndex: number = 0;
+  private totalSearchCount: number = 0;
 
   constructor(
     private userActions: UserActions,
     private componentDataStore: ComponentDataStore,
-    private element: ElementRef
+    private element: ElementRef,
+    private _ngZone: NgZone
   ) {
     this.nativeElement = element.nativeElement;
+
+    this.componentDataStore.dataStream
+      .subscribe((data: any) => {
+        this.totalSearchCount = data.totalSearchCount;
+      });
 
     this.componentDataStore.dataStream
       .filter((data) => {
@@ -39,6 +46,40 @@ export class TreeView {
         .subscribe((selectedNode: any) => {
           this.scrollToViewIfNeeded(selectedNode.nativeElement);
         });
+  }
+
+
+  /**
+   * Query for a node
+   * @param  {String} query
+   */
+  onChange(event, query, isNext) {
+
+    if (query.length === 0) {
+      return;
+    }
+
+    if (isNext === undefined && event.keyCode === 13) {
+      this.searchIndex++;
+    } else if (isNext === undefined) {
+      this.searchIndex = 0;
+    } else if (isNext) {
+      this.searchIndex++;
+    } else if (!isNext) {
+      this.searchIndex--;
+    }
+
+    // cycle over the search results if reached at the end
+    if (this.searchIndex === this.totalSearchCount) {
+      this.searchIndex = 0;
+    } else if (this.searchIndex < 0) {
+      this.searchIndex = this.totalSearchCount - 1;
+    }
+
+    query = query.toLocaleLowerCase();
+
+    this.userActions.searchNode({ query, index: this.searchIndex });
+    this._ngZone.run(() => undefined);
   }
 
   scrollToViewIfNeeded(elem: HTMLElement) {
