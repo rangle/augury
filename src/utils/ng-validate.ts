fe207baf067e1b558f2d7cccd256e319c708b712
“Sumit <er.sumitarora@gmail.com>
2016-06-09 20:57:59 -0400

    wait for updating dom if bootstrap takes long time

--- src/utils/ng-validate.ts
+++ src/utils/ng-validate.ts
@@ -1,4 +1,31 @@
-if (window && window.hasOwnProperty('ng')) {
-  window.postMessage({ type: 'AUGURY_NG_VALID' }, '*');
-}
+let isInitalized: boolean = false;
 
+const observeDOM = (() => {
+  const MutationObserver = window['MutationObserver'];
+  const eventListenerSupported = window.addEventListener;
+
+  return function (obj, callback) {
+    if (MutationObserver) {
+      let obs = new MutationObserver((mutations, observer) => {
+        if (mutations[0].addedNodes.length ||
+          mutations[0].removedNodes.length) {
+          callback();
+        }
+      });
+      obs.observe(obj, { childList: true, subtree: true });
+    } else if (eventListenerSupported) {
+      obj.addEventListener('DOMNodeInserted', callback, false);
+      obj.addEventListener('DOMNodeRemoved', callback, false);
+    }
+  };
+})();
+
+observeDOM(document, init);
+
+function init() {
+  if (window && window.hasOwnProperty('ng') && !isInitalized) {
+    isInitalized = true;
+    window.postMessage({ type: 'AUGURY_NG_VALID' }, '*');
+  }
+}
+init();
