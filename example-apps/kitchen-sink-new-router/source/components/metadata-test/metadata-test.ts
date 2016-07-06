014fcfe82bd70ce170ff825a7f52cc80941eff47
Eric Jimenez <eric.jimenez.dev@gmail.com>
2016-07-06 16:08:47 -0400

    fix: quotes on strings

--- example-apps/kitchen-sink-new-router/source/components/metadata-test/metadata-test.ts
+++ example-apps/kitchen-sink-new-router/source/components/metadata-test/metadata-test.ts
@@ -1,16 +1,16 @@
 import {Component, Input, Output} from '@angular/core';
 
 @Component({
-    selector: "attr-from-array",
-    inputs: ["attrIn"],
-    outputs: ["attrOut"],
+    selector: 'attr-from-array',
+    inputs: ['attrIn'],
+    outputs: ['attrOut'],
     template: `<p>{{attrIn}}</p>`
 })
 export class MetadataFromArray {
 }
 
 @Component({
-    selector: "attr-from-decorator",
+    selector: 'attr-from-decorator',
     template: `<p>{{attrIn}}</p>`
 })
 export class MetadataFromDecorator {
@@ -20,20 +20,20 @@ export class MetadataFromDecorator {
 
 
 @Component({
-    selector: "attr-from-array-custom",
-    inputs: ["attrIn:attrCustomIn"],
-    outputs: ["attrOut"],
+    selector: 'attr-from-array-custom',
+    inputs: ['attrIn:attrCustomIn'],
+    outputs: ['attrOut'],
     template: `<p>{{attrIn}}</p>`
 })
 export class MetadataFromArrayWithCustomName {
 }
 
 @Component({
-    selector: "attr-from-decorator-custom",
+    selector: 'attr-from-decorator-custom',
     template: `<p>{{attrIn}}</p>`
 })
 export class MetadataFromDecoratorWithCustomName {
-    @Input("attrCustomIn") attrIn: any;
+    @Input('attrCustomIn') attrIn: any;
     @Output() attrOut: any;
 }
 
@@ -41,17 +41,23 @@ export class MetadataFromDecoratorWithCustomName {
 @Component({
   selector: 'metadata-test',
   directives: [
-    MetadataFromArray, 
-    MetadataFromDecorator, 
-    MetadataFromArrayWithCustomName, 
+    MetadataFromArray,
+    MetadataFromDecorator,
+    MetadataFromArrayWithCustomName,
     MetadataFromDecoratorWithCustomName
   ],
   template: `
-    <attr-from-array attrIn="Data from the array."></attr-from-array>
-    <attr-from-decorator attrIn="Data from the decorator."></attr-from-decorator>
+    <attr-from-array attrIn='Data from the array.'>
+    </attr-from-array>
 
-    <attr-from-array-custom attrCustomIn="Data from array using a custom name"></attr-from-array-custom>
-    <attr-from-decorator-custom attrCustomIn="Data from decorator using a custom name"></attr-from-decorator-custom>
+    <attr-from-decorator attrIn='Data from the decorator.'>
+    </attr-from-decorator>
+
+    <attr-from-array-custom attrCustomIn='Data from array using a custom name.'>
+    </attr-from-array-custom>
+
+    <attr-from-decorator-custom attrCustomIn='Data from decorator using a custom name.'>
+    </attr-from-decorator-custom>
   `
 })
 export default class MetadataTest {
