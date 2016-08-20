11df8a5f60f7a2d123d16f090405a661317935fa
Chris Bond <chris.bond@rangle.io>
2016-08-20 17:46:23 -0400

    Note so I do not forget this thought later

--- src/communication/identifier.ts
+++ src/communication/identifier.ts
@@ -1,1 +1,4 @@
+// FIXME(cbond): This needs to trace a path back to the parent in order to have
+// optimal behaviour of the expansion / selection state. Even if a node changes
+// we can still select or highlight the right one by tracing the path.  TODO
 export const getUniqueIdentifier = () => Math.random().toString(16).slice(2);
