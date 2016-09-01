7a74845bc0ca086325b9c3518d40a8c2d0673a6e
Aaron Hamilton <aaron@correspondwith.me>
2016-09-01 14:37:55 +0000

    Remove deprecated router and the kitchen sink example which relies on it.

--- example-apps/kitchen-sink-example/source/services/service2.ts
+++
@@ -1,8 +0,0 @@
-import {Injectable} from '@angular/core';
-
-export default class Service2 {
-  value: string = 'service2';
-  constructor() {
-    this.value = this.value + ' Id: ' + Math.floor(Math.random() * 500);
-  }
-}
