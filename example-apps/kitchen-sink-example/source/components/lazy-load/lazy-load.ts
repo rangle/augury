d60057372f4c24509dfa198ab7c680cc5a2ac99a
Steven Sherrie <steve@wasteofpaper.com>
2016-10-18 16:38:36 +0100

    Fix spacing

--- example-apps/kitchen-sink-example/source/components/lazy-load/lazy-load.ts
+++ example-apps/kitchen-sink-example/source/components/lazy-load/lazy-load.ts
@@ -1,12 +1,12 @@
-import {Component} from '@angular/core';
-import { NgModule }           from '@angular/core';
-import { Routes, RouterModule }  from '@angular/router';
+import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 
 @Component({
   template: `<H1>Lazy Loaded Component One</H1>`
 })
-class LazyLoadedComponentOne { }
+class LazyLoadedComponentOne {}
 
 const routes: Routes = [
   { path: 'lazy1', component: LazyLoadedComponentOne },
