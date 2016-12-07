4be162d7fe2b6ede22b6c203d4d5ba9a12c102d5
Steven <steven@wasteofpaper.com>
2016-12-07 17:37:16 +0100

    Improve injector graph to show injected stuff only, with dep lines

--- src/frontend/components/injector-tree/injector-tree.ts
+++ src/frontend/components/injector-tree/injector-tree.ts
@@ -38,7 +38,6 @@ export class InjectorTree implements OnChanges {
   @Input() selectNode: EventEmitter<any>;
 
   private parentHierarchy;
-  private parentHierarchyDisplay;
   private svg: any;
 
   constructor(
@@ -56,35 +55,11 @@ export class InjectorTree implements OnChanges {
     }
   }
 
-  private addRootDependencies() {
-    const rootIndex = deserializePath(this.selectedNode.id).shift();
-
-    const rootElement = this.tree.roots[rootIndex];
-    if (rootElement == null) {
-      return;
-    }
-
-    this.selectedNode.dependencies.forEach(
-      (dependency: any) => {
-        if (this.selectedNode.injectors.indexOf(dependency.type) < 0) {
-          const parent = this.parseUtils.getDependencyLink
-            (this.tree, this.selectedNode.id, dependency.type);
-          if (!parent) {
-            rootElement.injectors.push(dependency.type);
-          }
-        }
-      });
-  }
-
   private displayTree() {
-    this.parentHierarchy = this.parseUtils.getParentHierarchy(this.tree, this.selectedNode,
-      node => {
-        return node.isComponent === true;
-      });
+    const mainHierarchy = this.parseUtils.getParentHierarchy(this.tree, this.selectedNode, node =>
+      node.isComponent === true);
 
-    this.parentHierarchyDisplay = this.parentHierarchy.concat([this.selectedNode]);
-
-    this.addRootDependencies();
+    this.parentHierarchy = [{ name: 'root', dependencies: [] }].concat(mainHierarchy).concat([this.selectedNode]);
 
     let firstChild: Element;
     while (firstChild = this.graphContainer.nativeElement.firstChild) {
@@ -99,24 +74,7 @@ export class InjectorTree implements OnChanges {
     this.render();
   }
 
-  private addPosition(positions: any, posX: number, posY: number, node: any, injector?: any) {
-    if (injector) {
-          positions[node.id].injectors[injector] = {
-            'x': posX,
-            'y': posY,
-            'injector': injector
-          };
-    } else {
-        positions[node.id] = {
-          'x': posX,
-          'y': posY,
-          'node': node,
-          'injectors': {}
-        };
-      }
-  }
-
-  private addNodeAndText(posX: number, posY: number, title: any, positions: any, clazz: string) {
+  private addNodeAndText(posX: number, posY: number, title: any, clazz: string) {
       this.graphUtils.addCircle(this.svg, posX, posY, NODE_RADIUS, clazz);
       this.graphUtils.addText(this.svg, posX - 6, posY - 15, title);
   }
@@ -127,100 +85,54 @@ export class InjectorTree implements OnChanges {
     }
 
     let posX, posY, x1, y1, x2, y2;
-    const positions = {};
 
-    let i: number = 0;
-    this.parentHierarchy.forEach((node) => {
+    this.parentHierarchy.forEach((node, hierarchyIdx: number) => {
       const nodeX = START_X;
-      const nodeY = START_Y + NODE_INCREMENT_Y * i;
+      const nodeY = START_Y + NODE_INCREMENT_Y * hierarchyIdx;
 
-      this.addPosition(positions, nodeX, nodeY, node);
+      node.dependencies.forEach((dependency, j: number) => {
+        const injectorX = nodeX + NODE_INCREMENT_X + NODE_INCREMENT_X * j;
 
-      let j: number = 0;
-      node.injectors.forEach((injector) => {
-        if (injector !== node.name) {
-          const injectorX = nodeX + NODE_INCREMENT_X + NODE_INCREMENT_X * j;
+        x1 = injectorX - NODE_INCREMENT_X + NODE_RADIUS;
+        y1 = nodeY;
+        x2 = injectorX - NODE_RADIUS;
+        y2 = nodeY;
+        this.graphUtils.addLine(this.svg, x1, y1, x2, y2, 'stroke-service');
 
-          x1 = injectorX - NODE_INCREMENT_X + NODE_RADIUS;
-          y1 = nodeY;
-          x2 = injectorX - NODE_RADIUS;
-          y2 = nodeY;
-          this.graphUtils.addLine(this.svg, x1, y1, x2, y2, 'stroke-service');
+        this.addNodeAndText(injectorX, nodeY, dependency.type, 'fill-service stroke-service');
 
-          this.addNodeAndText(injectorX, nodeY, injector, positions, 'fill-service stroke-service');
-          this.addPosition(positions, injectorX, nodeY, node, injector);
-
-          j++;
-        }
       });
 
-      if (i > 0) {
+      if (hierarchyIdx > 0) {
         x1 = nodeX;
-        y1 = START_Y + NODE_INCREMENT_Y * (i - 1) + NODE_RADIUS;
+        y1 = START_Y + NODE_INCREMENT_Y * (hierarchyIdx - 1) + NODE_RADIUS;
         x2 = nodeX;
         y2 = nodeY - (20 + NODE_RADIUS);
         this.graphUtils.addLine(this.svg, x1, y1, x2, y2, 'arrow stroke-component');
       }
-      this.addNodeAndText(nodeX, nodeY, node.name, positions, 'fill-component stroke-component');
 
-      i++;
-    });
-
-    let j: number = 0;
-    this.selectedNode.injectors.forEach((injector) => {
-      if (injector !== this.selectedNode.name) {
-        posX = START_X + NODE_INCREMENT_X + NODE_INCREMENT_X * j;
-        posY = START_Y + NODE_INCREMENT_Y * i;
-
-        x1 = posX - NODE_INCREMENT_X + NODE_RADIUS;
-        y1 = posY;
-        x2 = posX - NODE_RADIUS;
-        y2 = posY;
-        this.graphUtils.addLine(this.svg, x1, y1, x2, y2, 'stroke-service');
-
-        this.graphUtils.addCircle(this.svg, posX, posY, NODE_RADIUS, 'fill-service stroke-service');
-        this.graphUtils.addText(this.svg, posX - 6, posY - 15, injector);
-
-        j++;
-      }
-    });
-
-    posX = START_X;
-    posY = START_Y + NODE_INCREMENT_Y * i;
-    this.addNodeAndText(posX, posY, this.selectedNode.name, positions, 'fill-root stroke-root');
-    this.addPosition(positions, posX, posY, this.selectedNode);
-
-    if (i > 0) {
-      x1 = START_X;
-      y1 = START_Y + NODE_INCREMENT_Y * (i - 1) + NODE_RADIUS;
-      x2 = START_X;
-      y2 = posY - 28;
-      this.graphUtils.addLine(this.svg, x1, y1, x2, y2, 'arrow stroke-component');
-    }
-
-    this.selectedNode.dependencies.forEach((dependency: any) => {
-      const providedForSelf = this.selectedNode.providers.reduce((prev, curr, idx, p) =>
-        prev ? prev : p[idx].key === dependency.type && dependency.decorators.indexOf('@SkipSelf') < 0, false);
-      if (providedForSelf) {
-        return;
-      }
-
-      const parent = this.parseUtils.getDependencyLink
-        (this.tree, this.selectedNode.id, dependency.type);
-      if (parent) {
-        const service = positions[parent.id].injectors[dependency.type];
-        if (service) {
-          x1 = positions[this.selectedNode.id].x + 5;
-          y1 = positions[this.selectedNode.id].y - 10;
-          // Convert vector between the two nodes to desirable polar coordinates
-          const rho = Math.sqrt(((service.x - x1) ** 2) + ((y1 - service.y) ** 2)) - (NODE_RADIUS * 1.5);
-          const theta = Math.atan2(service.y - y1, service.x - x1);
-          // Convert back to euclidean coordinates
-          x2 = x1 + rho * Math.cos(theta);
-          y2 = y1 + rho * Math.sin(theta);
-          this.graphUtils.addLine(this.svg, x1, y1, x2, y2, 'stroke-root arrow dashed5');
+      // draw dependency links (if injectable was provided higher than current node)
+      node.dependencies.forEach((dependency: any, depIndex: number) => {
+        const providedForSelf = node.providers.reduce((prev, curr, idx, p) =>
+          prev ? prev : p[idx].key === dependency.type && dependency.decorators.indexOf('@SkipSelf') < 0, false);
+        if (providedForSelf) {
+          return;
         }
-      }
+
+        const parent = this.parseUtils.getDependencyLink(this.tree, node.id, dependency.type);
+
+        // no parent means 'root'. TODO:(steven.kampen) Improve with NgModule context
+        const parentIdx = !parent ? 0 : this.parentHierarchy.reduce((prev, curr, idx, p) =>
+          prev >= 0 ? prev : p[idx].name === parent.name ? idx : prev, -1);
+
+        x1 = START_X + NODE_INCREMENT_X * (depIndex + 1);
+        y1 = START_Y + hierarchyIdx * NODE_INCREMENT_Y;
+        x2 = START_X;
+        y2 = START_Y + NODE_INCREMENT_Y * parentIdx;
+
+        this.graphUtils.addLine(this.svg, x1, y1, x2, y2, 'stroke-dependency dashed5');
+      });
+      this.addNodeAndText(nodeX, nodeY, node.name, 'fill-component stroke-component');
     });
 
     this.svg.append('defs').selectAll('marker')
