83ae091980c68fe6e20c8f57727ecd61201e1956
Rajinder Yadav <rajindery@hotmail.com>
2017-02-08 11:41:38 -0500

    New feature, issue report creation. (#953)
    * Remove "Properties" accordion from component info panel
    
    * Fix nested & named outlet route paths
    
    * Parse router data
    
    * New feature, issue report creation.
    
    * Fixed const and spacing, PR review.

--- popup.js
+++ popup.js
@@ -1,17 +1,39 @@
+function initialize() {
+  subVersionNumbers();
+  document.getElementById("create_issue").addEventListener("click", generateGithubIssuesBodyText);
+}
 
 function subVersionNumbers() {
-
   var versionInstances = document.getElementsByClassName("version_number");
+  var version = chrome.runtime.getManifest().version || "1.1.0";
 
   for(var i = 0; i < versionInstances.length; i++){
-
-    var version = chrome.runtime.getManifest().version || "1.1.0";
     versionInstances[i].innerHTML = "(" + version + ")";
-
   }
 }
 
+function generateGithubIssuesBodyText() {
+  const date = (new Date()).toUTCString();
+  const body = `Augury: ${chrome.runtime.getManifest().version}
+Date: ${date}
+OS: ${navigator.platform}
 
-document.addEventListener('DOMContentLoaded', function () {
-  subVersionNumbers();
-});
+Demo Test Application
+Git repository for demo app showing issues (optional but very helpful for difficult issues).
+
+Description of issue:
+(Include images if possible)
+
+Angular Version: ???
+
+Steps to Reproduce:
+
+1.
+2.
+3.`;
+
+  window.open(`https://github.com/rangle/augury/issues/new?body=${window.encodeURI(body)}`);
+}
+
+document.addEventListener("DOMContentLoaded", initialize);
+
