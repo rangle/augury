52c9ad74bb3fa8553463446e39d2e0b98d261026
Jin Su Yoon <paulyoon1993@hotmail.com>
2017-06-07 10:18:26 -0400

    analytics extension work

--- src/frontend/epics/gtm.ts
+++ src/frontend/epics/gtm.ts
@@ -3,6 +3,9 @@ import {MainActions} from '../actions/main-actions';
 const DOM_SELECTION_GTM = 'auguryDOMSelection';
 const TAB_CHANGE_GTM = 'auguryTabChange';
 const SUB_TAB_CHANGE_GTM = 'augurySubTabChange';
+const EMIT_VALUE_GTM = 'auguryEmitValue';
+const UPDATE_PROPERTY_GTM = 'auguryUpdateProperty';
+const INITIALIZE_AUGURY_GTM = 'auguryInitialize';
 
 const TAB_TYPES = [
   'Component Tree',
@@ -49,4 +52,34 @@ export const subTabChangeGtmEpic = actions$ =>
           desc: SUB_TAB_TYPES[action.payload]
         }
       };
+    });
+
+export const emitValueGtmEpic = actions$ =>
+  actions$.ofType(MainActions.EMIT_VALUE)
+    .mapTo({
+      type: MainActions.SEND_ANALYTICS,
+      payload: {
+        event: EMIT_VALUE_GTM,
+        desc: 'Emit Clicked'
+      }
+    });
+
+export const updatePropertyGtmEpic = actions$ =>
+  actions$.ofType(MainActions.UPDATE_PROPERTY)
+    .mapTo({
+      type: MainActions.SEND_ANALYTICS,
+      payload: {
+        event: UPDATE_PROPERTY_GTM,
+        desc: 'Update property'
+      }
+    });
+
+export const initializeAuguryGtmEpic = actions$ =>
+  actions$.ofType(MainActions.INITIALIZE_AUGURY)
+    .mapTo({
+      type: MainActions.SEND_ANALYTICS,
+      payload: {
+        event: INITIALIZE_AUGURY_GTM,
+        desc: 'Initialize Augury'
+      }
     });
