69d34af7bdd294df2b067959498e5a75edf38555
santiago-elustondo <elus2012@gmail.com>
2018-02-24 00:21:38 -0500

    using Zones to map out execution stacks

--- src/diagnostic-tools/diagnoseFunction.function.ts
+++ src/diagnostic-tools/diagnoseFunction.function.ts
@@ -4,6 +4,8 @@ import * as clone from 'clone';
 
 import { DiagPacketConstructor } from './DiagPacket.class';
 
+declare const Zone: any;
+
 export function wrapFunction(
   end: 'backend'|'frontend',
   name: string, func: Function,
@@ -43,8 +45,17 @@ export function wrapFunction(
     }
 
     const { result, error } = (() => {
-      try { return { result: func.apply(this, args), error: undefined }; }
-      catch (error) { return { error, result: undefined }; }
+      let retVal;
+      if (!Zone.current.auguryDiagnostic) {
+          Zone.current.auguryDiagnostic = { stackLevel: 0 };
+      }
+      console.log('executing: ', name ? name : func.name);
+      console.log('current stack level: ', Zone.current.auguryDiagnostic.stackLevel);
+      Zone.current.auguryDiagnostic.stackLevel++;
+      try { retVal = { result: func.apply(this, args), error: undefined }; }
+      catch (error) { retVal = { error, result: undefined }; }
+      Zone.current.auguryDiagnostic.stackLevel--;
+      return retVal;
     })();
 
     if (!error) {
