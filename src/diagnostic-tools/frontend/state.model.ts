12d0f5dc4bc440454dedb239af373bb8d30658a0
santiago-elustondo <elus2012@gmail.com>
2018-03-01 15:34:01 -0500

    still broken

--- src/diagnostic-tools/frontend/state.model.ts
+++ src/diagnostic-tools/frontend/state.model.ts
@@ -3,7 +3,8 @@ const _getState = (store) => store[NAMESPACE];
 
 // @todo: needs type
 export const INITIAL_STATE = {
-  log: [] // @todo: needs type (msg/packet)
+  // log: [], // @todo: trying out different structure. keeping this as backup // @todo: needs type (msg/packet)
+  log: {} // @todo: rename as "logicalThreads"
 };
 
 export const selectors = {
@@ -12,6 +13,18 @@ export const selectors = {
   }
 };
 
+const asFrame = (packet) => ({ //@todo: types types types
+  children
+})
+
+const newThread = (fame) => ({
+    siblings: [],
+});
+
+const insertFrameIntoThread = (frame, thread = newThread(frame)) => {
+
+}
+
 export const updaters = {
   log: {
     addMsg (entry, state) { // @todo: type
@@ -19,11 +32,19 @@ export const updaters = {
       // return state.log.concat({type: 'MSG', value: entry });
     },
     addPkt (entry, state) { // @todo: type
+      const typedEntry = {type: 'PKT', value: entry};
+      if (state.log[entry.logicalThread.id]) {
+
+      } else {
+        state.log[entry.logicalThread.id] =
+      }
+      /* // @todo: trying out different structure. keeping this as backup
       return state.log.concat({type: 'PKT', value: entry })
         .sort((a, b) => { // @todo: shouldnt be sorting everything everytime
           if (a.type != 'PKT' || b.type != 'PKT') { return 0; }
           return (a.value.logicalThread.id < b.value.logicalThread.id) ? -1 : 1
         });
+        */
     }
   },
   clear () {
