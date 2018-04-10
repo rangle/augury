c3c0e52c3861b4cb43efd09fd8e04b0de7ba7b28
Andrew Thauer <andrew.thauer@rangle.io>
2018-04-10 14:32:40 -0400

    Upgrading to webpack 4

--- webpack.test.config.js
+++ webpack.test.config.js
@@ -1,63 +1,83 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const buildDir = path.join(__dirname, './build');
 
 module.exports = {
+  mode: 'development',
+
   entry: {
     'test': [
       path.join(__dirname, 'webpack.vendor.ts'),
-      path.join(__dirname, 'webpack.test.bootstrap.ts')
-    ]
+      path.join(__dirname, 'webpack.test.bootstrap.ts'),
+    ],
   },
 
   output: {
-    path: path.join(__dirname, './build'),
-    filename: '[name].js'
+    path: buildDir,
+    filename: '[name].js',
   },
+
   stats: {
     colors: true,
-    reasons: true
+    reasons: true,
   },
+
+  // Opt-in to the old behavior with the resolveLoader.moduleExtensions
+  // - https://webpack.js.org/guides/migrating/#automatic-loader-module-name-extension-removed
+  resolveLoader: {
+    moduleExtensions: ['-loader'],
+  },
+
   module: {
-    loaders: [{
-      // Support for .ts files.
-      test: /\.ts$/,
-      loader: 'ts',
-      exclude: /node_modules/,
-      query: {
-        'ignoreDiagnostics': []
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: [
+          /node_modules/
+        ],
       },
-      exclude: [
-        /node_modules/
-      ]
-    }, {
-      test: /\.css$/,
-      loader: 'css!postcss'
-    }, {
-      test: /\.png$/,
-      loader: "url-loader?mimetype=image/png"
-    }, {
-      test: /\.html$/,
-      loader: 'raw'
-    }],
+      {
+        test: /\.css$/,
+        use: [
+          'to-string-loader',
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.png$/,
+        use: 'url-loader?mimetype=image/png',
+      },
+      {
+        test: /\.html$/,
+        use: 'raw-loader',
+      },
+    ],
+
     noParse: [
       /rtts_assert\/src\/rtts_assert/,
       /reflect-metadata/,
       /.+zone\.js\/dist\/.+/,
-      /.+angular2\/bundles\/.+/
-    ]
+      /.+angular2\/bundles\/.+/,
+    ],
   },
+
   resolve: {
-    extensions: ['', '.ts', '.js', '.jsx'],
-    modulesDirectories: ['src', 'node_modules']
+    extensions: ['.ts', '.js', '.jsx'],
+    modules: ['src', 'node_modules'],
   },
 
   node: {
-    'fs': 'empty'
+    'fs': 'empty',
   },
 
   plugins: [
     new webpack.DefinePlugin({
       chrome: '{runtime: {connect: function() {}}}'
-    })
-  ]
+    }),
+    new CleanWebpackPlugin(buildDir),
+  ],
 };
