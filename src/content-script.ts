75ce0960d4b685c66b94050b055e2e1dae71421d
Andrei Petcu <andreicristianpetcu@users.noreply.github.com>
2018-04-30 22:30:14 +0300

    Fix firefox xml viewer (#1290)
    * Release (1.16.0) (#1284)
    
    * fix bug: frontend doesnt clear error screen when app detected (#1274)
    
    * FireFox compatibility using WebExtensions format. (#1268)
    
    * modify manifest for compatibility
    
    * add web extensions packing
    
    * remove augury.xpi
    
    * fix popup size in firefox
    
    * browser-specific builds
    
    * browser-specific builds
    
    * manifest.json merging & npm script naming scheme
    
    * uncomment sentry_key check
    
    * add uglify
    
    * add canary to builds
    
    * add canary to builds
    
    * version up
    
    * add yihua fix
    
    * linter
    
    * version up
    
    * Updating docs and text to reflect Firefox support (#1278)
    
    * small patch for PrimeNG
    
    * linter
    
    * version up
    
    * fix dependencies thing yet again
    
    * version up
    
    * include canary build in ISSUE_TEMPLATE.md
    
    * Update README.md with canary
    
    * remove comma
    
    * version up for release (manifest)
    
    * version up for release (package)
    
    * remove update_url from manifest.json for chrome
    
    * Update CHANGELOG (1.16.0)
    
    * tag version
    
    * #1289: set script.textContent only if the document is HTML (Avoids breaking Firefox XML formatter)

--- src/content-script.ts
+++ src/content-script.ts
@@ -43,50 +43,52 @@ export const injectSettings = (options: SimpleOptions) => {
   });
 };
 
-browserSubscribeOnce(MessageType.FrameworkLoaded,
-  () => {
-    loadOptions().then(options => {
-      // We want to load the tree rendering options that the UI has saved
-      // because that allows us to send the correct tree immediately upon
-      // startup and send it to the message queue, allowing Augury to render
-      // instantly as soon as the application is loaded. Without this bit
-      // of code we would have to wait for the frontend to start and load its
-      // options and then request the tree, which would add a lot of latency
-      // to startup.
-      injectSettings(options);
+if (document instanceof HTMLDocument) {
+  browserSubscribeOnce(MessageType.FrameworkLoaded,
+    () => {
+      loadOptions().then(options => {
+        // We want to load the tree rendering options that the UI has saved
+        // because that allows us to send the correct tree immediately upon
+        // startup and send it to the message queue, allowing Augury to render
+        // instantly as soon as the application is loaded. Without this bit
+        // of code we would have to wait for the frontend to start and load its
+        // options and then request the tree, which would add a lot of latency
+        // to startup.
+        injectSettings(options);
 
-      injectScript('build/backend.js');
+        injectScript('build/backend.js');
+      });
+
+      return true;
     });
 
-    return true;
+  browserSubscribeDispatch((message: any) => {
+    if (message.messageType === MessageType.DispatchWrapper) {
+      send(message.content)
+        .then(response => {
+          messageJumpContext(MessageFactory.response(message, response, true));
+        })
+        .catch(error => {
+          messageJumpContext(MessageFactory.response(message, error, false));
+        });
+    }
   });
 
-browserSubscribeDispatch((message: any) => {
-  if (message.messageType === MessageType.DispatchWrapper) {
-    send(message.content)
-      .then(response => {
-        messageJumpContext(MessageFactory.response(message, response, true));
-      })
-      .catch(error => {
-        messageJumpContext(MessageFactory.response(message, error, false));
-      });
-  }
-});
+  subscribe((message: Message<any>) => messageJumpContext(message));
 
-subscribe((message: Message<any>) => messageJumpContext(message));
+  send(MessageFactory.initialize())
+    .then((response: { extensionId: string }) => {
+      injectScript('build/ng-validate.js');
+    })
+    .catch(error => {
+      console.error('Augury initialization has failed', error);
+    });
 
-send(MessageFactory.initialize())
-  .then((response: {extensionId: string}) => {
-    injectScript('build/ng-validate.js');
-  })
-  .catch(error => {
-    console.error('Augury initialization has failed', error);
-  });
-
-const propertyKey = '$$el';
-const warningText = `$$el will only be set in the 'top' execution context, \
+  const propertyKey = '$$el';
+  const warningText = `$$el will only be set in the 'top' execution context, \
 which you can select via the dropdown in the console pane \
 (https://developers.google.com/web/tools/chrome-devtools/console/\
 #execution-context).`;
 
-Object.defineProperty(window, propertyKey, { value: warningText });
+  Object.defineProperty(window, propertyKey, {value: warningText});
+}
