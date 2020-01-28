6f1b46f2ae06a0464117525dd596ed6cfd265086
Yvonne Allen <yallen011@gmail.com>
2020-01-28 10:16:59 -0500

    Currently Angular applications using ivy will not have support for the injector graph so we are removing the injector graph view right now for those applications.

--- src/frontend/components/components-tab-menu/components-tab-menu.ts
+++ src/frontend/components/components-tab-menu/components-tab-menu.ts
@@ -3,41 +3,49 @@ import {
   EventEmitter,
   Input,
   Output,
+  OnInit,
+  AfterContentInit,
+  AfterViewInit,
+  OnChanges
 } from '@angular/core';
-import {
-  InstanceWithMetadata,
-  Metadata,
-  Node,
-  ObjectType,
-  Path,
-} from '../../../tree';
+import { InstanceWithMetadata, Metadata, Node, ObjectType, Path } from '../../../tree';
 
-import {StateTab} from '../../state';
+import { StateTab } from '../../state';
 
 export interface StateTabDescription {
   title: string;
   tab;
 }
 
-import {UserActions} from '../../actions/user-actions/user-actions';
+import { UserActions } from '../../actions/user-actions/user-actions';
 
 @Component({
   selector: 'bt-components-tab-menu',
-  templateUrl: './components-tab-menu.html',
+  templateUrl: './components-tab-menu.html'
 })
-export class ComponentsTabMenu {
+export class ComponentsTabMenu implements OnChanges {
   @Input() selectedStateTab;
+  @Input() isIvy: boolean;
   @Output() tabChange: EventEmitter<any> = new EventEmitter<any>();
 
-  tabs: Array<StateTabDescription> = [{
-    title: 'Properties',
-    tab: StateTab.Properties,
-  }, {
-    title: 'Injector Graph',
-    tab: StateTab.InjectorGraph,
-  }];
+  tabs: Array<StateTabDescription> = [
+    {
+      title: 'Properties',
+      tab: StateTab.Properties
+    },
+    {
+      title: 'Injector Graph',
+      tab: StateTab.InjectorGraph
+    }
+  ];
 
-  constructor(private userActions: UserActions) {
+  constructor(private userActions: UserActions) {}
+
+  ngOnChanges(changes: import('@angular/core').SimpleChanges): void {
+    //remove injector graph tab from applications that use Angular Ivy
+    if (this.isIvy) {
+      this.tabs.pop();
+    }
   }
 
   private onSelect(tab: StateTabDescription) {
--- src/frontend/components/info-panel/info-panel.ts
+++ src/frontend/components/info-panel/info-panel.ts
@@ -1,28 +1,17 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  Output,
-} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
-import {ComponentLoadState, StateTab} from '../../state';
-import {UserActions} from '../../actions/user-actions/user-actions';
-import {
-  ComponentMetadata,
-  InstanceWithMetadata,
-  Metadata,
-  Node,
-  ObjectType,
-  Path,
-} from '../../../tree';
+import { ComponentLoadState, StateTab } from '../../state';
+import { UserActions } from '../../actions/user-actions/user-actions';
+import { ComponentMetadata, InstanceWithMetadata, Metadata, Node, ObjectType, Path } from '../../../tree';
 
 @Component({
   selector: 'bt-info-panel',
-  templateUrl: './info-panel.html',
+  templateUrl: './info-panel.html'
 })
 export class InfoPanel {
   @Input() tree;
-  @Input() ngModules: {[key: string]: any};
+  @Input() ngModules: { [key: string]: any };
+  @Input() isIvy: boolean;
   @Input() node;
   @Input() instanceValue: InstanceWithMetadata;
   @Input() loadingState: ComponentLoadState;
@@ -30,8 +19,8 @@ export class InfoPanel {
 
   @Output() selectNode: EventEmitter<any> = new EventEmitter<any>();
   @Output() componentsSubTabMenuChange: EventEmitter<StateTab> = new EventEmitter<StateTab>();
-  @Output() emitValue = new EventEmitter<{path: Path, data: any}>();
-  @Output() updateProperty = new EventEmitter<{path: Path, newValue: any}>();
+  @Output() emitValue = new EventEmitter<{ path: Path; data: any }>();
+  @Output() updateProperty = new EventEmitter<{ path: Path; newValue: any }>();
 
   StateTab = StateTab;
 
@@ -45,20 +34,14 @@ export class InfoPanel {
   }
 
   get metadata(): Metadata {
-    return this.instanceValue
-      ? this.instanceValue.metadata
-      : new Map<string, [ObjectType, any]>();
+    return this.instanceValue ? this.instanceValue.metadata : new Map<string, [ObjectType, any]>();
   }
 
-  get providers(): {[token: string]: any} {
-    return this.instanceValue
-      ? this.instanceValue.providers
-      : {};
+  get providers(): { [token: string]: any } {
+    return this.instanceValue ? this.instanceValue.providers : {};
   }
 
   get componentMetadata(): ComponentMetadata {
-    return this.instanceValue
-      ? this.instanceValue.componentMetadata
-      : new Map<string, [[string, ObjectType, any]]>();
+    return this.instanceValue ? this.instanceValue.componentMetadata : new Map<string, [[string, ObjectType, any]]>();
   }
 }
